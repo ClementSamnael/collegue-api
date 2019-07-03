fadb8a976fe4535036f16451c91987668e503b6e
Clement ORMAUX <clement.ormaux@gmail.com>
2019-07-03 17:28:53 +0200

    Commit Gestion Exception

--- src/main/java/com/example/demo/service/CollegueService.java
+++ src/main/java/com/example/demo/service/CollegueService.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import java.util.UUID;
 
 import com.example.demo.entite.Collegue;
+import com.example.demo.exception.CollegueNonTrouverException;
 
 public class CollegueService {
 
@@ -21,21 +22,31 @@ public class CollegueService {
         String mat3 = UUID.randomUUID().toString();
         data.put(mat1, new Collegue(mat1, "Durand", "Amandine", "amandine.durand@societe.com",
                 LocalDate.of(1991, 9, 21), "photo.png"));
-        data.put(mat2, new Collegue(mat2, "Robert", "Clement", "clement.robert@societe.com",
-                LocalDate.of(1992, 11, 11), "photo.png"));
-        data.put(mat3, new Collegue(mat3, "Leroy", "Yoann", "yoann.leroy@societe.com",
-                LocalDate.of(1987, 1, 14), "photo.png"));
+        data.put(mat2, new Collegue(mat2, "Robert", "Clement", "clement.robert@societe.com", LocalDate.of(1992, 11, 11),
+                "photo.png"));
+        data.put(mat3, new Collegue(mat3, "Leroy", "Yoann", "yoann.leroy@societe.com", LocalDate.of(1987, 1, 14),
+                "photo.png"));
     }
 
     public List<Collegue> rechercherParNom(String nomRecherche) {
         // TODO retourner une liste de collègues dont le nom est fourni
         List<Collegue> collegues = new ArrayList<Collegue>();
         for (Map.Entry<String, Collegue> collegue : data.entrySet()) {
-            if (nomRecherche.equals(collegue.getValue().getNom())){
-                collegues.add(collegue.getValue());  
+            if (nomRecherche.equals(collegue.getValue().getNom())) {
+                collegues.add(collegue.getValue());
             }
         }
         return collegues;
+    }
 
+    public Collegue rechercherParMatricule(String matriculeRecherche) throws CollegueNonTrouverException {
+        // TODO retourner le collègue dont le matricule est fourni
+        if (data.containsKey(matriculeRecherche))
+            throw new CollegueNonTrouverException();
+
+        // TODO retourner une exception `CollegueNonTrouveException` (à créer)
+        // si le matricule ne correspond à aucun collègue
+
+        return data.get(matriculeRecherche);
     }
 }
